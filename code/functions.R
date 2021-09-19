945263653502e0c23af421c6a28a37c2bef34040
robwschlegel <robwschlegel@gmail.com>
2021-09-19 06:14:22 -0300

    Shifting file pathways

--- code/functions.R
+++ code/functions.R
@@ -100,11 +100,11 @@ lon_lat_OISST <- base::expand.grid(lon_OISST, lat_OISST) %>%
 # File locations
 OISST_files <- dir("../data/OISST", pattern = "avhrr-only", full.names = T)
 MCS_files <- dir("../data/MCS", full.names = T)
-MCS_lon_files <- dir("../data/cat_lon/MCS", full.names = T)
-MCS_cat_files <- dir("../data/cat_clim/MCS", full.names = T, recursive = T)
-MCS_event_files <- dir("../data/event/MCS", full.names = T, pattern = "MCS")
+MCS_lon_files <- dir("../data/cat_lon_MCS", full.names = T)
+MCS_cat_files <- dir("../data/cat_clim_MCS", full.names = T, recursive = T)
+MCS_event_files <- dir("../data/event_MCS", full.names = T, pattern = "MCS")
 MCS_count_trend_files <- dir("annual_summary_MCS", pattern = "count_trend", full.names = T)
-MCS_seas_thresh_files <- dir("../data/thresh/MCS", pattern = "MCS.seas.thresh.", full.names = T)
+MCS_seas_thresh_files <- dir("../data/thresh_MCS", pattern = "MCS.seas.thresh.", full.names = T)
 
 # Metadata
 load("metadata/OISST_ocean_coords.Rdata")
